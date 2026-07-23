c971701b8614e71e602ab44602392624bebc8071
bukit3point0 <github@billiereynolds.com>
2026-07-23 00:19:23 -0500

    working countdown timer

---
+++ scripts/clock.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var level_time = 300 # 5m in seconds as a default for now
+
+@onready var clock_time: RichTextLabel = $Sprite2D/MarginContainer/RichTextLabel
+
+func _ready() -> void:
+	countdown()
+
+func countdown() -> void:
+	set_time_on_clock()
+	while level_time > 0:
+		level_time -= 1
+		set_time_on_clock()
+		await get_tree().create_timer(1.0).timeout
+
+func set_time_on_clock() -> void:
+	var minutes = int(floor(level_time / 60.0))
+	var seconds = str(level_time % 60)
+	if minutes < 10:
+		minutes = "0" + str(minutes)
+	else:
+		minutes = str(minutes)
+	clock_time.text = minutes + ":" + seconds
+	
