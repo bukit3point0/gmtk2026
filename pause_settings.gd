a7b307f589ea59259defcf8ea3e2bf7572007ccc
Birdsinhats <stray1532@gmail.com>
2026-07-23 18:03:10 -0500

    fixed the signals on pause menu

---
+++ pause_settings.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var settings = get_node("Settings")
+var game_root
+signal restart_signal
+
+func _ready() -> void:
+	game_root = owner.get_parent()
+	connect("restart_signal", Callable(game_root, "load_game"))
+
+#opposite of pause
+func _on_resume_button_pressed() -> void:
+	var game_script = owner
+	game_script.unpause()
+	visible = false
+
+#kill current scene, reinstatiate it and add it to game root and restart time
+func _on_restart_level_button_pressed() -> void:
+	emit_signal("restart_signal")
+
+func _on_settings_button_pressed() -> void:
+	settings.visible = true
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
