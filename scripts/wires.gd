2df6b6dbae6af2fb36c62b7622a986d613b4eba5
bukit3point0 <github@billiereynolds.com>
2026-07-25 16:36:48 -0500

    masking tape labels on wires

--- scripts/wires.gd
+++ scripts/wires.gd
@@ -1,23 +1,25 @@
-extends CharacterBody2D
+extends Node2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var wire_1_red: Node2D = $Wire1Red
+@onready var wire_2_blue: Node2D = $Wire2Blue
+@onready var wire_1_yellow: Node2D = $Wire1Yellow
+@onready var wire_1_green: Node2D = $Wire1Green
+@onready var wire_1_purple: Node2D = $Wire1Purple
+@onready var wire_1_black: Node2D = $Wire1Black
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+# mock wire setup
+var wire_selections = [3,4,5,0,1,3]
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _ready() -> void:
+	wire_1_red.get_child(wire_selections[0]).visible = true
+	wire_2_blue.get_child(wire_selections[1]).visible = true
+	wire_1_yellow.get_child(wire_selections[2]).visible = true
+	wire_1_green.get_child(wire_selections[3]).visible = true
+	wire_1_purple.get_child(wire_selections[4]).visible = true
+	wire_1_black.get_child(wire_selections[5]).visible = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _on_button_pressed() -> void:
+	print("click")
 
-	move_and_slide()
+func _on_wire_length_1_pressed() -> void:
+	print("click2")
