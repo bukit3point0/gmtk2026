3b3573e06bbb7150cf8ab4142a8bc27658457d18
Cody <CodyEthanJordan@users.noreply.github.com>
2026-07-25 17:35:11 -0500

    Able to toggle switches visually

--- scripts/lightswitch_board.gd
+++ scripts/lightswitch_board.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-@onready var lightbulb: TextureRect = $GridContainer/Lightbulb
+@onready var lightbulb_1: TextureRect = $GridContainer/Lightbulb
 @onready var lightbulb_2: TextureRect = $GridContainer/Lightbulb2
 @onready var lightbulb_3: TextureRect = $GridContainer/Lightbulb3
 @onready var lightbulb_4: TextureRect = $GridContainer/Lightbulb4
 @onready var lightbulb_5: TextureRect = $GridContainer/Lightbulb5
-@onready var switch: TextureRect = $GridContainer/Switch
+@onready var switch_1: TextureRect = $GridContainer/Switch
 @onready var switch_2: TextureRect = $GridContainer/Switch2
 @onready var switch_3: TextureRect = $GridContainer/Switch3
 @onready var switch_4: TextureRect = $GridContainer/Switch4
 @onready var switch_5: TextureRect = $GridContainer/Switch5
 
+@onready var lights = [lightbulb_1, lightbulb_2, lightbulb_3, lightbulb_4, lightbulb_5]
+@onready var switches: Array[TextureRect] = [switch_1, switch_2, switch_3, switch_4, switch_5]
+
 var unlit_bulb = preload("uid://ctl34c38murvo")
 var lit_bulb = preload("uid://den8akhwdtr4x")
-var switch_off = preload("uid://bcigmnpnb71u1")
-var switch_on = preload("uid://djdavkyeuyedt")
+var switch_on = preload("uid://bcigmnpnb71u1")
+var switch_off = preload("uid://djdavkyeuyedt")
 
 var switches_on = [false, false, false, false, false]
 
@@ -24,4 +27,10 @@ func _ready() -> void:
 func _on_switch_button_pressed(button_index) -> void:
 	print("button pressed")
 	print(button_index)
-	pass # Replace with function body.
+
+	switches_on[button_index] = !switches_on[button_index]
+
+	if switches_on[button_index]:
+		switches[button_index].texture = switch_on
+	else:
+		switches[button_index].texture = switch_off
