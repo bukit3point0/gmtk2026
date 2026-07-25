62a9c8940d2b6d7ed56c6e3476d27a5c9f3bfdf6
bukit3point0 <github@billiereynolds.com>
2026-07-25 15:21:33 -0500

    push file

---
+++ scripts/game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func pause_trigger():
+	var clock_node = get_node("Level1/Background/TextureRect/Clock")
+	clock_node.pause_countdown = true
+	var pauseMenu = get_node("PauseMenu")
+	print("pause ", pauseMenu)
+	pauseMenu.visible = true
+
+func unpause():
+	var clock_node = get_node("Level1/Background/TextureRect/Clock")
+	clock_node.pause_countdown = false
+	clock_node.countdown()
+
+func _process(_delta: float) -> void:
+	# If escape is pressed it triggers the func to pause timer and open pause menu
+	if Input.is_action_just_pressed("Escape"):
+		pause_trigger()
