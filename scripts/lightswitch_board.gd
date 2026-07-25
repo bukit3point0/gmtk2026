3fcb4186bde6aabec3b1def168ac7c14d50a9f69
Cody <CodyEthanJordan@users.noreply.github.com>
2026-07-25 16:39:52 -0500

    Update button pressing logic for lightswitches, change how signals are hooked up

--- scripts/lightswitch_board.gd
+++ scripts/lightswitch_board.gd
@@ -11,43 +11,17 @@ extends Node2D
 @onready var switch_4: TextureRect = $GridContainer/Switch4
 @onready var switch_5: TextureRect = $GridContainer/Switch5
 
+var unlit_bulb = preload("uid://ctl34c38murvo")
+var lit_bulb = preload("uid://den8akhwdtr4x")
+var switch_off = preload("uid://bcigmnpnb71u1")
+var switch_on = preload("uid://djdavkyeuyedt")
+
 var switches_on = [false, false, false, false, false]
 
 func _ready() -> void:
-	update_lights()
+	pass
 
-func update_lights() -> void:
-	turn_light_on(lightbulb, switch, switches_on[0])
-	turn_light_on(lightbulb_2, switch_2, switches_on[1])
-	turn_light_on(lightbulb_3, switch_3, switches_on[2])
-	turn_light_on(lightbulb_4, switch_4, switches_on[3])
-	turn_light_on(lightbulb_5, switch_5, switches_on[4])
-
-func turn_light_on(lightbulb: TextureRect, switch: TextureRect, on: bool = true) -> void:
-	var unlit_bulb = preload("uid://ctl34c38murvo")
-	var lit_bulb = preload("uid://den8akhwdtr4x")
-	var switch_off = preload("uid://bcigmnpnb71u1")
-	var switch_on = preload("uid://djdavkyeuyedt")
-	
-	lightbulb.texture = lit_bulb if on else unlit_bulb
-	switch.texture = switch_on if on else switch_off
-
-func _on_switch_1_pressed() -> void:
-	switches_on[0] = !switches_on[0]
-	update_lights()
-
-func _on_switch_2_pressed() -> void:
-	switches_on[1] = !switches_on[1]
-	update_lights()
-
-func _on_switch_3_pressed() -> void:
-	switches_on[2] = !switches_on[2]
-	update_lights()
-
-func _on_switch_4_pressed() -> void:
-	switches_on[3] = !switches_on[3]
-	update_lights()
-
-func _on_switch_5_pressed() -> void:
-	switches_on[4] = !switches_on[4]
-	update_lights()
+func _on_switch_button_pressed(button_index) -> void:
+	print("button pressed")
+	print(button_index)
+	pass # Replace with function body.
