ef050457268355c3857de0548e5e83bd818c4735
bukit3point0 <github@billiereynolds.com>
2026-07-25 14:40:35 -0500

    arrow right

---
+++ scripts/construction_report_page.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var construction_report_dragging = false
+
+signal construction_report_drag_signal
+
+func _ready() -> void:
+	connect("construction_report_drag_signal", Callable(self, "_set_construction_report_drag"))
+
+func _process(_delta: float) -> void:
+	if construction_report_dragging:
+		var mouse_position = get_viewport().get_mouse_position()
+		position = Vector2(mouse_position.x, mouse_position.y)
+		move_and_slide()
+
+func _set_construction_report_drag() -> void:
+	construction_report_dragging = !construction_report_dragging
+
+func _on_construction_report_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	print("input")
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			emit_signal("construction_report_drag_signal")
+		elif event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
+			emit_signal("construction_report_drag_signal")
+	elif event is InputEventScreenTouch:
+		if event.pressed and event.get_index() == 0:
+			self.position = event.get_position()
