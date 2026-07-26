bd091e3aed10b92c961cb2de2bc982bda2419c99
Cody <CodyEthanJordan@users.noreply.github.com>
2026-07-25 22:08:27 -0500

    Temp test protocol

--- scripts/thermometer.gd
+++ scripts/thermometer.gd
@@ -1,17 +1,38 @@
 extends CharacterBody2D
 
+const FRAME_COUNT = 18
+
 var thermometer_dragging = false
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var noise_timer: float = 0
+var target_temp: float
 
 signal thermometer_drag_signal
 
 func _ready() -> void:
 	connect("thermometer_drag_signal", Callable(self, "_set_thermometer_drag"))
+	EventBus.connect("_set_temp", _on_set_temp)
 
-func _process(_delta: float) -> void:
+
+func _process(delta: float) -> void:
 	if thermometer_dragging:
 		var mouse_position = get_viewport().get_mouse_position()
 		position = Vector2(mouse_position.x, mouse_position.y)
 		move_and_slide()
+	
+	noise_timer -= delta
+	if noise_timer <= 0:
+		noise_timer = randf_range(0.1, 0.2)
+		var twiddled_temp = target_temp + randf_range(-0.1, 0.1)
+		var frame = floor(twiddled_temp * FRAME_COUNT)
+		frame = min(frame, FRAME_COUNT)
+		frame = max(frame, 0)
+		sprite.frame = frame
+
+func _on_set_temp(temp: float):
+	target_temp = temp
+	sprite.frame = floor(temp * FRAME_COUNT)
 
 func _set_thermometer_drag() -> void:
 	thermometer_dragging = !thermometer_dragging
